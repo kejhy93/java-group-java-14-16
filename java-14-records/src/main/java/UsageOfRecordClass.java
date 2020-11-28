60ff57b859793f0c1f026d78a280fa0533068d69
Lukáš Hejnal <kejhy93@gmail.com>
2020-11-28 12:03:41 +0100

    Java14-Records-Example with equals method

--- java-14-records/src/main/java/UsageOfRecordClass.java
+++ java-14-records/src/main/java/UsageOfRecordClass.java
@@ -6,5 +6,14 @@ public class UsageOfRecordClass {
         System.out.println("a.firstName=" + a.firstName());
         System.out.println("a.firstName=" + a.lastName());
         System.out.println("a.toString \"" + a.toString() + "\"");
+
+        RecordClass b = new RecordClass("C", "D");
+        RecordClass c = new RecordClass("A", "B");
+        
+        System.out.println("====== RecordClass equals ==========");
+        System.out.println("equals(a,b) = equals(\"" + a.toString() + "\", \"" + b.toString() +
+                "\") = " + a.equals(b));
+        System.out.println("equals(a,c) = equals(\"" + a.toString() + "\", \"" + c.toString() +
+                "\") = " + a.equals(c));
     }
 }
