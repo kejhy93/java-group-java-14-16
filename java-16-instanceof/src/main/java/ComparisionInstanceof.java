c000f7d271eaad06057a3d1c0133281b2d571400
Lukáš Hejnal <kejhy93@gmail.com>
2021-03-04 11:24:01 +0100

    Changes

--- java-16-instanceof/src/main/java/ComparisionInstanceof.java
+++ java-16-instanceof/src/main/java/ComparisionInstanceof.java
@@ -1,10 +1,13 @@
 public class ComparisionInstanceof {
-    
+
     class PointLegacy {
         private int x;
         private int y;
 
-        public PointLegacy(int x, int y) {this.x=x;this.y=y;}
+        public PointLegacy(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
         public boolean equals(Object o) {
             if (!(o instanceof PointLegacy))
@@ -18,12 +21,13 @@ public class ComparisionInstanceof {
         private int x;
         private int y;
 
-        public PointNew(int x, int y) {this.x=x;this.y=y;}
+        public PointNew(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
         public boolean equals(Object o) {
-            return (o instanceof PointNew other)
-                && x == other.x
-                && y == other.y;
+            return (o instanceof PointNew other) && x == other.x && y == other.y;
         }
     }
 }
