1a8d3f2e14bfae9b5a760d89fa7d587f361e7306
Lukáš Hejnal <kejhy93@gmail.com>
2021-03-04 11:08:26 +0100

    Add examples for Sealed classes

---
+++ java-16-sealed-classes/src/main/java/Shape.java
@@ -0,0 +1,5 @@
+public abstract sealed class Shape
+    permits Circle, Rectangle, Square {
+
+
+}
---
+++ java-16-sealed-classes/src/main/java/expr/Expr.java
@@ -0,0 +1,7 @@
+public sealed interface Expr
+    permits ConstantExpr, PlusExpr, TimesExpr, NegExpr {  }
+
+record ConstantExpr(int i)       implements Expr {  }
+record PlusExpr(Expr a, Expr b)  implements Expr {  }
+record TimesExpr(Expr a, Expr b) implements Expr {  }
+record NegExpr(Expr e)           implements Expr {  }
