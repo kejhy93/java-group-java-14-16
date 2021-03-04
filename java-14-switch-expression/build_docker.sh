27b3cf7fb3ab9cec6cf5d790bb2c7918510a1c1b
Lukáš Hejnal <kejhy93@gmail.com>
2021-03-04 11:19:56 +0100

    Changes

--- java-14-switch-expression/build_docker.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-TAG="javagroupjava1416:java-14-switch"
-
-echo "Start building Java 14 switch image"
-
-docker build --pull --rm -f "Dockerfile" -t ${TAG} .
-
-docker run --rm -it ${TAG}
