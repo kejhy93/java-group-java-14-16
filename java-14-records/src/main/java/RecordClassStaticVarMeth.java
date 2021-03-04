c80d84c04befdba803867111c2331046534df82c
Lukáš Hejnal <kejhy93@gmail.com>
2021-03-04 11:38:27 +0100

    Changes

--- java-14-records/src/main/java/RecordClassStaticVarMeth.java
+++ java-14-records/src/main/java/RecordClassStaticVarMeth.java
@@ -1,4 +1,4 @@
-public record PersonStat (String name, String address) {
+public record RecordClassStaticVarMeth (String name, String address) {
     public static String UNKNOWN_ADDRESS = "Unknown";
 
     public static void PersonStat generateUnnamedPerson(String address) {
